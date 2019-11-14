a79b62fb7c70c0f0bd0b8feb9e9470c065c06cad
CodingTil <36734749+CodingTil@users.noreply.github.com>
2019-11-14 19:03:27 +0100

    Adding JavaDoc to Kreise

--- Kreise/Circle.java
+++ Kreise/Circle.java
@@ -1,16 +1,17 @@
 /**
  * Ein Objekt der Klasse Circle repräsentiert einen Kreis.
+ * @author Til Mohr
  */
 public class Circle {
 
     private int x, y, radius;
 
     /**
-     * Beschreibung der Methode newCircle(int x, int y, int radius)
+     * Erstellen eines neuen Circle-Objekts
      * @param x
      * @param y
      * @param radius
-     * @return
+     * @return Das neue Circle-Objekt
      */
     public static Circle newCircle(int x, int y, int radius) {
 		Circle circle = new Circle();
@@ -21,9 +22,9 @@ public class Circle {
 	}
 
     /**
-     * Beschreibung der Methode newCircle(int radius)
+     * Erstellen eines neuen Circle-Objekts
      * @param radius
-     * @return
+     * @return Das neue Circle-Objekt
      */
     public static Circle newCircle(int radius) {
 		Circle circle = new Circle();
@@ -34,23 +35,23 @@ public class Circle {
 	}
 
     /**
-     * Beschreibung der Methode clone()
-     * @return
+     * Klont ein Circle-Objekt
+     * @return Gibt das geklonte Circle-Object zurück
      */
     public Circle clone() {
         return newCircle(getX(), getY(), getRadius());
     }
 
     /**
-     * Beschreibung der Methode getRadius()
-     * @return
+	 * Bekomme den Radius
+     * @return Radius
      */
     public int getRadius() {
         return radius;
     }
 
     /**
-     * Beschreibung der Methode setRadius(int radius)
+     * Setze den Radius
      * @param radius
      */
     public void setRadius(int radius) {
@@ -62,15 +63,15 @@ public class Circle {
     }
 
     /**
-     * Beschreibung der Methode getX()
-     * @return
+	 * Bekomme die X-Koordinate
+     * @return X-Koordinate
      */
     public int getX() {
 		return x;
     }
 
     /**
-     * Beschreibung der Methode setX(int x)
+     * Setze die X-Koordinate
      * @param x
      */
     public void setX(int x) {
@@ -78,15 +79,15 @@ public class Circle {
     }
 
     /**
-     * Beschreibung der Methode getY()
-     * @return
+     * Bekomme die Y-Koordinate
+     * @return Y-Koordinate
      */
     public int getY() {
         return y;
     }
 
     /**
-     * Beschreibung der Methode setY(int y)
+     * Setze die Y-Koordinate
      * @param y
      */
     public void setY(int y) {
@@ -94,7 +95,7 @@ public class Circle {
     }
 
     /*
-     * Grobe Beschreibung der Methode (private, muss nicht ausführlich dokumentiert werden)
+     * Sagt, ob der Kreis den anderen Kreis vollständig beinhaltet.
      */
     private boolean containsOne(Circle that) {
         return Utils.dist(that.getX(), that.getY(), getX(), getY()) + that.getRadius() >= getRadius();
@@ -119,11 +120,11 @@ public class Circle {
     }
 
     /**
-     * Beschreibung der Methode circumscriber(int x, int y, Circle... circles)
+     * Erstellt einen neuen Kreis an der Stelle (x|y) und minimalem radius, sodass alle anderen Kreis enthalten sind.
      * @param x
      * @param y
      * @param circles
-     * @return
+     * @return den Kreis (x|y), minimaler radius
      */
     public static Circle circumscriber(int x, int y, Circle... circles) {
         int radius = 0;
@@ -144,16 +145,16 @@ public class Circle {
     }
 
     /**
-     * Beschreibung der Methode performAction()
+     * Zur Kontrolle eines Kreises: Verschieben in x-/y-Richtung und Radiusvergrößerung/-verkleinerung
      * @param action
      */
     public void performAction(CircleAction action) {
         switch(action) {
 			case UP:
-				setY(getY() + 10);
+				setY(getY() - 10);
 				break;
 			case DOWN:
-				setY(getY() - 10);
+				setY(getY() + 10);
 				break;
 			case LEFT:
 				setX(getX() - 10);
